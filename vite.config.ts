ea07efbe4c44b874a6c05efaeb7fdd0268c03d4b
Ashish Talati <ashish.talati@gmail.com>
2023-03-16 08:22:01 -0500

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: "../backend/static",
+        outDir: "backend/static",
         emptyOutDir: true,
         sourcemap: true
     }
