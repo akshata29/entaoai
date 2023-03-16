273968d701ddf2c451f82a74d4c098ca8d0f69b5
Ashish Talati <ashish.talati@gmail.com>
2023-03-16 08:49:06 -0500

    Refactor directory

---
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -0,0 +1,43 @@
+import { Outlet, NavLink, Link } from "react-router-dom";
+
+import github from "../../assets/github.svg";
+
+import styles from "./Layout.module.css";
+
+const Layout = () => {
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header} role={"banner"}>
+                <div className={styles.headerContainer}>
+                    <Link to="/" className={styles.headerTitleContainer}>
+                        <h3 className={styles.headerTitle}>Chat PDF</h3>
+                    </Link>
+                    <nav>
+                        <ul className={styles.headerNavList}>
+                            <li className={styles.headerNavLeftMargin}>
+                                <NavLink to="/upload" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                Upload PDF&nbsp;&nbsp;&nbsp;
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Chat
+                                </NavLink>
+                            </li>
+                            <li className={styles.headerNavLeftMargin}>
+                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Ask a question
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                    <h4 className={styles.headerRightText}>Azure OpenAI</h4>
+                </div>
+            </header>
+
+            <Outlet />
+        </div>
+    );
+};
+
+export default Layout;
